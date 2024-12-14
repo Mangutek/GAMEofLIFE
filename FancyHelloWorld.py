12656ff10b17c4ce3e5c1168ce536394077d5303
Franciszek Żółtogórski <152714905+Mangutek@users.noreply.github.com>
2024-12-14 19:32:42 +0100

    Add files via upload

---
+++ FancyHelloWorld.py
@@ -0,0 +1,14 @@
+import os, time
+symbols = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','w','y','z', ' ']
+result = "hello world"
+current = ""
+while current != result:
+    for char in result:
+        for symbol in symbols:
+            print(current + symbol)
+            time.sleep(0.05)
+            os.system('cls')
+            if symbol == char:
+                current = current + symbol
+                break
+print(result)
