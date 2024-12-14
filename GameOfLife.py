1f79a1aa68054d5070160934f1dc4866c0a291bb
Franciszek Żółtogórski <152714905+Mangutek@users.noreply.github.com>
2024-12-14 19:20:02 +0100

    Add files via upload

---
+++ GameOfLife.py
@@ -0,0 +1,85 @@
+import os
+import time
+import numpy as np
+tekst = open("C:\\Users\\Admin\\Desktop\\GAMEOFLIFE\\data.txt").readlines()
+data = []
+for line in tekst: data.append([])
+for y in range(len(tekst)):
+    for x in range(len(tekst[0])-1):
+        data[y].append(tekst[y][x])
+
+def copymatrix(new, old):
+    for z in range(len(old)):
+        new.append([])
+    for y in range(len(old)):
+        for x in range(len(old[y])):
+             new[y].append(old[y][x])
+def printmatrix(matrix):
+    print(len(matrix), len(matrix[0]))
+    string = ""
+    for y in range(len(matrix)):
+        for x in range(len(matrix[0])):
+            string = string + str(matrix[y][x]) + " "
+        print(string)
+        string = ""
+
+def game_of_life(dane):
+    tab = []
+    tab.clear()
+    copymatrix(tab, dane)
+    for y in range(len(dane)):
+        for x in range(len(dane[y])):
+            ile = 0
+            try:
+                if dane[y-1][x-1] == 'X': ile=ile+1
+            except:
+                pass
+            try:
+                if dane[y-1][x] == 'X': ile=ile+1
+            except:
+                pass
+            try:
+                if dane[y-1][x+1] == 'X': ile=ile+1
+            except:
+                pass
+            try:
+                if dane[y][x-1] == 'X': ile=ile+1
+            except:
+                pass
+            try:
+                if dane[y][x+1] == 'X': ile=ile+1
+            except:
+                pass
+            try:
+                if dane[y+1][x-1] == 'X': ile=ile+1
+            except:
+                pass
+            try:
+                if dane[y+1][x] == 'X': ile=ile+1
+            except:
+                pass
+            try:
+                if dane[y+1][x+1] == 'X': ile=ile+1
+            except:
+                pass
+            if dane[y][x] == 'X' and (ile == 2 or ile == 3):
+                tab[y][x] = 'X'
+            elif dane[y][x] == '.' and ile == 3:
+                tab[y][x] = 'X'
+            else:
+                tab[y][x] = '.'
+    return tab
+
+for i in range(100):
+    print(i)
+    printmatrix(data)
+    data = game_of_life(data)
+    time.sleep(0.5)
+    os.system('cls')
+
+
+
+
+    
+            
+        
